3b9fe7d3f0e23d8e843a6db2bc9d6dc96e23eb80
Kai Takac <kai.takac@gmail.com>
2017-12-26 18:30:06 +0100

    MOD extract oAuth configuration information into platform

--- OBankingConnector/Domain/BankServiceProviders/Gustav/GustavBankServiceProviderConfiguration.swift
+++ OBankingConnector/Domain/BankServiceProviders/Gustav/GustavBankServiceProviderConfiguration.swift
@@ -8,58 +8,19 @@
 
 import Foundation
 
-public struct GustavBankServiceProviderConfiguration: OAuth2BankServiceConfiguration {
+public struct GustavBankServiceProviderConfiguration: BankServiceProviderConfiguration {
 
     public let bankServiceProvider: BankServiceProvider = GustavBankServiceProvider()
 
     let clientId: String
     let clientSecret: String?
     let redirectURI: String?
-
-    // swiftlint:disable force_unwrapping
-    let authorizationEndpointURL: URL = URL(string: "https://www.csas.cz/widp/oauth2/auth")!
-    let tokenEndpointURL: URL? = URL(string: "https://www.csas.cz/widp/oauth2/token")!
-    // swiftlint:enable force_unwrapping
-
-    let scope: String? = nil
-    let additionalAuthorizationRequestParameters: [String: String]? = [
-        "access_type": "offline"
-    ]
-    let additionalTokenRequestParameters: [String: String]? = nil
-    let additionalHeaders: [String: String]?
-    let bankingRequestTranslator: BankingRequestTranslator
-
-    var authorizationServerCertificate: Data {
-        return self.apiServerCertificate
-    }
-
-    var tokenServerCertificate: Data {
-        return self.apiServerCertificate
-    }
-
-    // swiftlint:disable force_try
-    var apiServerCertificate: Data {
-        guard let certificateURL = OBankingConnector.bundle.url(forResource: "wwwcsascz", withExtension: "crt") else {
-            fatalError("Could not read certificate")
-        }
-
-        return try! Data(contentsOf: certificateURL)
-    }
-    // swiftlint:enable force_try
+    let webAPIKey: String
 
     public init(clientId: String, clientSecret: String, redirectURI: String, webAPIKey: String) {
         self.clientId = clientId
         self.clientSecret = clientSecret
         self.redirectURI = redirectURI
-
-        self.additionalHeaders = [
-            "web-api-key": webAPIKey
-        ]
-
-        guard let baseURL = URL(string: "https://www.csas.cz/webapi/api/v3") else {
-            fatalError("Cannot create base URL")
-        }
-
-        self.bankingRequestTranslator = GustavBankingRequestTranslator(baseURL: baseURL)
+        self.webAPIKey = webAPIKey
     }
 }
--- OBankingConnector/Domain/BankServiceProviders/Gustav/GustavSandboxBankServiceProviderConfiguration.swift
+++ OBankingConnector/Domain/BankServiceProviders/Gustav/GustavSandboxBankServiceProviderConfiguration.swift
@@ -8,60 +8,19 @@
 
 import Foundation
 
-/// See https://github.com/Ceskasporitelna/WebAPI/wiki/The-integration-of-the-WebAPI-in-the-CS
-public struct GustavSandboxBankServiceProviderConfiguration: OAuth2BankServiceConfiguration {
+public struct GustavSandboxBankServiceProviderConfiguration: BankServiceProviderConfiguration {
 
     public let bankServiceProvider: BankServiceProvider = GustavBankServiceProvider()
 
     let clientId: String
     let clientSecret: String?
     let redirectURI: String?
-
-    // swiftlint:disable force_unwrapping
-    let authorizationEndpointURL: URL = URL(string: "https://www.csast.csas.cz/widp/oauth2/auth")!
-    let tokenEndpointURL: URL? = URL(string: "https://www.csast.csas.cz/widp/oauth2/token")!
-    // swiftlint:enable force_unwrapping
-
-    let scope: String? = nil
-    let additionalAuthorizationRequestParameters: [String: String]? = [
-        "access_type": "offline"
-    ]
-    let additionalTokenRequestParameters: [String: String]? = nil
-    let additionalHeaders: [String: String]?
-    let bankingRequestTranslator: BankingRequestTranslator
-
-    var authorizationServerCertificate: Data {
-        return self.apiServerCertificate
-    }
-
-    var tokenServerCertificate: Data {
-        return self.apiServerCertificate
-    }
-
-    // swiftlint:disable force_try
-    var apiServerCertificate: Data {
-        let bundle = OBankingConnector.bundle
-        guard let certificateURL = bundle.url(forResource: "wwwcsastcsascz", withExtension: "crt") else {
-            fatalError("Could not read certificate")
-        }
-
-        return try! Data(contentsOf: certificateURL)
-    }
-    // swiftlint:enable force_try
+    let webAPIKey: String
 
     public init(clientId: String, clientSecret: String, redirectURI: String, webAPIKey: String) {
         self.clientId = clientId
         self.clientSecret = clientSecret
         self.redirectURI = redirectURI
-
-        self.additionalHeaders = [
-            "web-api-key": webAPIKey
-        ]
-
-        guard let baseURL = URL(string: "https://www.csast.csas.cz/webapi/api/v3") else {
-            fatalError("Cannot create base URL")
-        }
-
-        self.bankingRequestTranslator = GustavBankingRequestTranslator(baseURL: baseURL)
+        self.webAPIKey = webAPIKey
     }
 }
